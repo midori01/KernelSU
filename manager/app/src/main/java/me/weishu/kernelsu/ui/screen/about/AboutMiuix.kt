ac263d8b4962f653215936a8ed30eb56b2d1ec08
KOWX712 <leecc0503@gmail.com>
2026-04-05 19:07:52 +0800

    manager: new app icon
    Thanks to ZG089 for the design
    
    Co-Authored-By: 々𝗭𝗚𝗫 丨「#ɴᴇᴠᴇʀɢɪᴠᴇᴜᴘ」 <159061718+zg089@users.noreply.github.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/about/AboutMiuix.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/about/AboutMiuix.kt
@@ -40,7 +40,6 @@ import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.HazeStyle
 import dev.chrisbanes.haze.HazeTint
 import dev.chrisbanes.haze.hazeSource
-import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.theme.LocalEnableBlur
 import me.weishu.kernelsu.ui.util.defaultHazeEffect
 import top.yukonga.miuix.kmp.basic.Card
@@ -131,7 +130,7 @@ fun AboutScreenMiuix(
                             .background(Color.White)
                     ) {
                         Image(
-                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            painter = painterResource(id = state.appIconRes),
                             contentDescription = null,
                             contentScale = FixedScale(1f)
                         )
