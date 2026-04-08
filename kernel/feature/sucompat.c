a86fcbe484f5da69ed3366a10d8abee209aaed31
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-08 12:29:43 +0800

    kernel: tiny_sulog: basic ringbuffer, timestamped su log
    This is a very basic ringbuffer implementation.
    on this one we keep 8 bytes per entry and 250 entries max.
    
    This logs escalation access about: who did it, what happened and when did
    it happen.
    
    userspace can deduce as we also send next index to it. which makes it so
    that this is also the oldest entry. next -1 is the latest entry.
    
    v2: timestamped version
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -25,6 +25,8 @@
 #define SU_PATH "/system/bin/su"
 #define SH_PATH "/system/bin/sh"
 
+extern void write_sulog(uint8_t sym);
+
 bool ksu_su_compat_enabled __read_mostly = true;
 
 static int su_compat_feature_get(u64 *value)
@@ -84,6 +86,7 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
     strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
     if (unlikely(!memcmp(path, su, sizeof(su)))) {
+        write_sulog('a');
         pr_info("faccessat su->sh!\n");
         *filename_user = sh_user_path();
     }
@@ -109,6 +112,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
     strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
     if (unlikely(!memcmp(path, su, sizeof(su)))) {
+        write_sulog('s');
         pr_info("newfstatat su->sh!\n");
         *filename_user = sh_user_path();
     }
@@ -146,6 +150,7 @@ long ksu_handle_execve_sucompat(const char __user **filename_user, int orig_nr, 
     if (likely(memcmp(path, su, sizeof(su))))
         goto do_orig_execve;
 
+    write_sulog('x');
     pr_info("sys_execve su found\n");
     pending_sucompat = ksu_sulog_capture_sucompat(*filename_user, argv_user, GFP_KERNEL);
     *filename_user = ksud_user_path();
