bd03c877f87265b8f600adf3cdd3541f213e5829
KOWX712 <leecc0503@gmail.com>
2026-05-10 22:28:26 +0800

    manager: QOL
    - default to material ui
    - bring back old animation spec
    - remove warning card
    - sort module by name, allow action and webui even when module is flagged as update
    - U-LEGACY and LEGACY working mode
    - don't always show nav bar label
    - preload app list and module list
    - allow update check even module already flagged update

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
@@ -1,5 +1,6 @@
 package me.weishu.kernelsu.ui.screen.home
 
+import android.os.Build
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
@@ -85,17 +86,17 @@ fun HomePagerMaterial(
             } else if (state.showKernelPrBuildWarning) {
                 WarningCard(stringResource(id = R.string.home_pr_kernel_warning))
             }
-            if (state.showVersionMismatchWarning) {
-                WarningCard(
-                    stringResource(id = R.string.home_version_mismatch,
-                        state.currentManagerVersionCode,
-                        state.ksuVersion ?: 0
-                    )
-                )
-            }
-            if (state.showGkiWarning) {
-                WarningCard(stringResource(id = R.string.home_gki_warning))
-            }
+//            if (state.showVersionMismatchWarning) {
+//                WarningCard(
+//                    stringResource(id = R.string.home_version_mismatch,
+//                        state.currentManagerVersionCode,
+//                        state.ksuVersion ?: 0
+//                    )
+//                )
+//            }
+//            if (state.showGkiWarning) {
+//                WarningCard(stringResource(id = R.string.home_gki_warning))
+//            }
             if (state.showRequireKernelWarning) {
                 WarningCard(
                     stringResource(id = R.string.require_kernel_version,
@@ -195,7 +196,7 @@ private fun StatusCard(
                 when {
                     state.ksuVersion != null -> {
                         val workingMode = when (state.lkmMode) {
-                            null -> ""
+                            null -> if (Build.SUPPORTED_64_BIT_ABIS.isEmpty()) "32-BIT" else "LEGACY"
                             true -> "LKM"
                             else -> "GKI"
                         }
