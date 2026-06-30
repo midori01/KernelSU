1a941ded1b665ee87520ae92e73d5da4f06f5fac
midori01 <lv@lvlv.lv>
2026-06-30 10:25:06 +0900

    extra
    Signed-off-by: midori01 <lv@lvlv.lv>

--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -692,6 +692,65 @@ static int do_disable_escape_to_root(void __user *arg)
 	return 0;
 }
 
+static int do_get_hook_type(void __user *arg)
+{
+    struct ksu_hook_type_cmd cmd = { 0 };
+
+#if defined(CONFIG_KSU_HACK_ARM64_BRANCH_LINK)
+    const char *type = "Branch with Link Hijack";
+#elif defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
+    const char *type = "Syscall Table Tamper";
+#elif defined(CONFIG_KSU_KPROBES_KSUD)
+    #ifdef CONFIG_KSU_SUSFS
+        const char *type = "De-inlined SUSFS / Hybrid";
+    #else
+        const char *type = "Hybrid";
+    #endif
+#else
+    #ifdef CONFIG_KSU_SUSFS
+        const char *type = "De-inlined SUSFS / Manual";
+    #else
+        const char *type = "Manual";
+    #endif
+#endif
+
+    strscpy(cmd.hook_type, type, sizeof(cmd.hook_type));
+
+    if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+        pr_err("get_hook_type: copy_to_user failed\n");
+        return -EFAULT;
+    }
+
+    return 0;
+}
+
+static int do_get_susfs_version(void __user *arg)
+{
+    struct ksu_susfs_version_cmd cmd = { 0 };
+
+#ifdef CONFIG_KSU_SUSFS
+    strscpy(cmd.version, SUSFS_VERSION, sizeof(cmd.version));
+#else
+    strscpy(cmd.version, "Not supported", sizeof(cmd.version));
+#endif
+
+    if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+        return -EFAULT;
+    }
+    return 0;
+}
+
+static int do_get_driver_name(void __user *arg)
+{
+    struct ksu_driver_name_cmd cmd = { 0 };
+    strscpy(cmd.name, "XX", sizeof(cmd.name));
+
+    if (copy_to_user(arg, &cmd, sizeof(cmd))) {
+        return -EFAULT;
+    }
+    return 0;
+}
+
 // IOCTL handlers mapping table
 static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
 	{ .cmd = KSU_IOCTL_GRANT_ROOT, .name = "GRANT_ROOT", .handler = do_grant_root, .perm_check = allowed_for_su },
@@ -718,6 +777,9 @@ static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
 	{ .cmd = KSU_IOCTL_SET_INIT_PGRP, .name = "SET_INIT_PGRP", .handler = do_set_init_pgrp, .perm_check = only_root },
 	{ .cmd = KSU_IOCTL_GET_SULOG_FD, .name = "GET_SULOG_FD", .handler = do_get_sulog_fd, .perm_check = only_root },
 	{ .cmd = KSU_IOCTL_DISABLE_ESCAPE_TO_ROOT, .name = "DISABLE_ESCAPE_TO_ROOT", .handler = do_disable_escape_to_root, .perm_check = only_root },
+	{ .cmd = KSU_IOCTL_HOOK_TYPE, .name = "HOOK_TYPE", .handler = do_get_hook_type, .perm_check = manager_or_root },
+	{ .cmd = KSU_IOCTL_SUSFS_VERSION, .name = "SUSFS_VERSION", .handler = do_get_susfs_version, .perm_check = manager_or_root },
+	{ .cmd = KSU_IOCTL_DRIVER_NAME, .name = "DRIVER_NAME", .handler = do_get_driver_name, .perm_check = manager_or_root },
 	{ .cmd = 0, .name = NULL, .handler = NULL, .perm_check = NULL } // Sentinel
 };
 
