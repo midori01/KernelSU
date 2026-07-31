e290a98288f85d584dd8969273fe4df791c7486f
midori01 <lv@lvlv.lv>
2026-07-31 20:09:07 +0900

    susfs
    Signed-off-by: midori01 <lv@lvlv.lv>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -147,6 +147,10 @@
 #include "hook/kp_ksud.c"
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 struct cred* ksu_cred;
 
 extern void ksu_supercalls_init();
@@ -244,6 +248,10 @@ static int __init kernelsu_init(void)
 	ksu_allowlist_init();
 
 	ksu_throne_tracker_init();
+
+#ifdef CONFIG_KSU_SUSFS
+    susfs_init();
+#endif // #ifdef CONFIG_KSU_SUSFS
 
 	ksu_ksud_init();
 
