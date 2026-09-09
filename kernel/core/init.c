ffc10155910e8c3e21ca30aaa488cf377d5c1e46
midori01 <lv@lvlv.lv>
2026-09-09 09:09:43 +0900

    decouple
    Signed-off-by: midori01 <lv@lvlv.lv>

--- kernel/core/init.c
+++ kernel/core/init.c
@@ -114,13 +114,16 @@ static inline void __init ksu_hook_init(void)
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 8, 0)
     ksu_lsm_hook_built_in_init();
 #endif
-#elif defined(CONFIG_KSU_SUSFS)
+#elif !defined(CONFIG_KSU_TRACEPOINT_HOOK) && !defined(CONFIG_KSU_MANUAL_HOOK) && defined(CONFIG_KSU_SUSFS)
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 8, 0)
     ksu_lsm_hook_built_in_init();
 #endif
-    susfs_init();
 #else
 #error "Unsupported hook type"
+#endif
+
+#ifdef CONFIG_KSU_SUSFS
+    susfs_init();
 #endif
 }
 
