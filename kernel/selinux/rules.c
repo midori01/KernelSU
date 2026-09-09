d929a603d285a4e45dbe42058e69f3f400a12dda
midori01 <lv@lvlv.lv>
2026-09-09 18:38:28 +0900

    de-inlined susfs
    Signed-off-by: midori01 <lv@lvlv.lv>

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -40,6 +40,9 @@ static void reset_avc_cache()
     selinux_status_update_policyload(&selinux_state, 0);
 #endif
     selinux_xfrm_notify_policyload();
+#ifdef CONFIG_KSU_SUSFS
+    susfs_set_batch_sid();
+#endif // #ifdef CONFIG_KSU_SUSFS
 }
 
 void apply_kernelsu_rules()
--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -22,6 +22,9 @@
 #include "sulog/event.h"
 #include "sulog/fd.h"
 #include "supercall/supercall.h"
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif // #ifdef CONFIG_KSU_SUSFS
 
 static int do_grant_root(void __user *arg)
 {
@@ -134,6 +137,9 @@ static int do_report_event(void __user *arg)
             } else {
                 pr_info("boot_complete triggered\n");
                 on_boot_completed();
+#ifdef CONFIG_KSU_SUSFS
+                susfs_start_sdcard_monitor_fn();
+#endif // #ifdef CONFIG_KSU_SUSFS
             }
         }
         break;
