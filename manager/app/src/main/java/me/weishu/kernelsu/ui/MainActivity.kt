e68e842da38af3785eb44d0d51cfb1e4b670009a
midori01 <lv@lvlv.lv>
2026-09-15 18:01:49 +0900

    [midori] manager: allow switching kernel tool in bottom bar
    Signed-off-by: midori01 <lv@lvlv.lv>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -94,6 +94,7 @@ import me.weishu.kernelsu.ui.screen.dmesg.DmesgScreen
 import me.weishu.kernelsu.ui.screen.superuser.SuperUserPager
 import me.weishu.kernelsu.ui.screen.template.AppProfileTemplateScreen
 import me.weishu.kernelsu.ui.screen.templateeditor.TemplateEditorScreen
+import me.weishu.kernelsu.ui.component.bottombar.KernelTool
 import me.weishu.kernelsu.ui.screen.kconfig.KconfigScreen
 import me.weishu.kernelsu.ui.screen.kallsyms.KallsymsScreen
 import me.weishu.kernelsu.ui.screen.kernelmodule.KernelModuleScreen
@@ -196,7 +197,8 @@ open class MainActivity : ComponentActivity() {
                 LocalUiMode provides uiMode,
                 LocalShowSwitchIcon provides appSettings.showSwitchIcon,
                 LocalScrollAnimation provides appSettings.scrollAnimation,
-                LocalClassicUi provides appSettings.classicUi
+                LocalClassicUi provides appSettings.classicUi,
+                LocalKernelTool provides uiState.bottomBarKernelTool,
             ) {
                 KernelSUTheme(appSettings = appSettings, uiMode = uiMode) {
                     IntentDispatcher(intentChannel = intentChannel)
@@ -288,6 +290,7 @@ open class MainActivity : ComponentActivity() {
     }
 }
 
+val LocalKernelTool = staticCompositionLocalOf { KernelTool.Kconfig }
 val LocalMainPagerState = staticCompositionLocalOf<MainPagerState> { error("LocalMainPagerState not provided") }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -513,7 +516,13 @@ fun MainPage(
     when (page) {
         0 -> HomePager(navigator, bottomInnerPadding, isCurrentPage)
         1 -> SuperUserPager(navigator, bottomInnerPadding, isCurrentPage)
-        2 -> KconfigScreen()
+        2 -> when (LocalKernelTool.current) {
+            KernelTool.Kconfig -> KconfigScreen(isRootTab = true)
+            KernelTool.Dmesg -> DmesgScreen(isRootTab = true)
+            KernelTool.CrashLog -> CrashLogScreen(isRootTab = true)
+            KernelTool.Kallsyms -> KallsymsScreen(isRootTab = true)
+            KernelTool.KernelModule -> KernelModuleScreen(isRootTab = true)
+        }
         3 -> ModulePager(bottomInnerPadding, isCurrentPage)
         4 -> SettingPager(navigator, bottomInnerPadding, isCurrentPage)
     }
--- manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/MainActivityViewModel.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/MainActivityViewModel.kt
@@ -11,6 +11,7 @@ import me.weishu.kernelsu.data.repository.SettingsRepository
 import me.weishu.kernelsu.data.repository.SettingsRepositoryImpl
 import me.weishu.kernelsu.ksuApp
 import me.weishu.kernelsu.ui.UiMode
+import me.weishu.kernelsu.ui.component.bottombar.KernelTool
 import me.weishu.kernelsu.ui.theme.ThemeController
 
 class MainActivityViewModel(
@@ -51,6 +52,7 @@ class MainActivityViewModel(
             enableFloatingBottomBarBlur = settingRepo.enableFloatingBottomBarBlur,
             enableNavigationBadge = settingRepo.enableNavigationBadge,
             uiMode = UiMode.fromValue(settingRepo.uiMode),
+            bottomBarKernelTool = KernelTool.fromId(settingRepo.bottomBarKernelTool),
         )
     }
 
@@ -70,6 +72,7 @@ class MainActivityViewModel(
             "enable_floating_bottom_bar_blur",
             "enable_navigation_badge",
             "ui_mode",
+            KernelTool.PREF_KEY,
         )
     }
 }
