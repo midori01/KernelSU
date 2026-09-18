4ac0b2f330cf78f95360e86ee2a3b7ec175437f7
midori01 <lv@lvlv.lv>
2026-09-18 20:03:38 +0900

    [midori] ksud: update banner and cli branding for MidoriSU
    Signed-off-by: midori01 <lv@lvlv.lv>
    (cherry picked from commit 3756bd080897fa691fd622bfa2a2175e71737e12)

--- userspace/ksud/src/installer.sh
+++ userspace/ksud/src/installer.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 ############################################
-# KernelSU installer script
+# MidoriSU installer script
 # mostly from module_installer.sh
 # and util_functions.sh in Magisk
 ############################################
@@ -403,7 +403,7 @@ install_module() {
     set_permissions
   else
     print_title "$MODNAME" "by $MODAUTH"
-    print_title "Powered by KernelSU"
+    print_title "Powered by MidoriSU"
 
     unzip -o "$ZIPFILE" customize.sh -d $MODPATH >&2
 
