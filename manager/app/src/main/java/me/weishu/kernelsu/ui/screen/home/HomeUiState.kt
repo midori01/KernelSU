57aaa09ba694235ddc43a900feef836dff08a123
midori01 <lv@lvlv.lv>
2026-09-22 15:58:53 +0900

    [midori] manager: redesign home screen status dashboard
    Signed-off-by: midori01 <lv@lvlv.lv>
    (cherry picked from commit 2e92bc41ff0a8d2473255fe66409cd0ab1cfe81a)

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeUiState.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeUiState.kt
@@ -3,6 +3,13 @@ package me.weishu.kernelsu.ui.screen.home
 import androidx.compose.runtime.Immutable
 import me.weishu.kernelsu.KernelVersion
 import me.weishu.kernelsu.ui.util.module.LatestVersionInfo
+import me.weishu.kernelsu.ui.screen.home.toRoman
+
+data class LatestKsuDriverInfo(
+    val tag: String = "",
+    val driverVersion: Int = 0,
+    val releaseUrl: String = "",
+)
 
 @Immutable
 data class HomeUiState(
@@ -26,7 +33,13 @@ data class HomeUiState(
     val checkUpdateEnabled: Boolean,
     val latestVersionInfo: LatestVersionInfo,
     val currentManagerVersionCode: Long,
+    val superuserCount: Int,
+    val moduleCount: Int,
+    val kernelModuleCount: Int,
     val systemInfo: SystemInfo,
+    val isGki2: Boolean,
+    val localVersion: String,
+    val latestKsuDriverInfo: LatestKsuDriverInfo = LatestKsuDriverInfo(),
 ) {
     val isSELinuxPermissive: Boolean
         get() = systemInfo.selinuxStatus == "Permissive"
@@ -46,6 +59,12 @@ data class HomeUiState(
     val showRootWarning: Boolean
         get() = ksuVersion != null && !isRootAvailable
 
+    val isFullFeatured: Boolean
+        get() = isManager && isRootAvailable
+
+    val showRequireKernelWarning: Boolean
+        get() = false
+
     val showManagerPrBuildWarning: Boolean
         get() = isManager && isManagerPrBuild
 
@@ -54,11 +73,20 @@ data class HomeUiState(
 
     val hasUpdate: Boolean
         get() = latestVersionInfo.versionCode > currentManagerVersionCode
+
+    val formattedManagerUAPIVersion: String
+        get() = managerUAPIVersion.toRoman()
+
+    val formattedKernelUAPIVersion: String
+        get() = kernelUAPIVersion?.toRoman() ?: "N"
 }
 
 @Immutable
 data class HomeActions(
     val onInstallClick: () -> Unit,
+    val onSuperuserClick: () -> Unit,
+    val onModuleClick: () -> Unit,
     val onOpenUrl: (String) -> Unit,
+    val onKernelModuleClick: () -> Unit,
     val onJailbreakClick: () -> Unit = {},
 )
