62b60a9c5534f5606971da15480742d13ba0aba8
midori01 <lv@lvlv.lv>
2026-09-22 15:58:53 +0900

    [midori] manager: add kallsyms symbol viewer
    Signed-off-by: midori01 <lv@lvlv.lv>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/navigation3/Routes.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/navigation3/Routes.kt
@@ -83,4 +83,8 @@ sealed interface Route : NavKey, Parcelable {
     @Parcelize
     @Serializable
     data class ExecuteModuleAction(val moduleId: String, val fromShortcut: Boolean = false) : Route
+
+    @Parcelize
+    @Serializable
+    data object Kallsyms : Route
 }
