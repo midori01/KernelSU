b7fe56f7bcb10e2a493845516d1c52a4cf92d6be
midori01 <lv@lvlv.lv>
2026-09-22 15:58:54 +0900

    [midori] manager: replace navigation badge with module update badge
    Signed-off-by: midori01 <lv@lvlv.lv>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/MainActivityUiState.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/MainActivityUiState.kt
@@ -12,7 +12,7 @@ data class MainActivityUiState(
     val enableBlur: Boolean,
     val enableFloatingBottomBar: Boolean,
     val enableFloatingBottomBarBlur: Boolean,
-    val enableNavigationBadge: Boolean,
+    val enableModuleUpdateBadge: Boolean,
     val enableSwipeDismiss: Boolean,
     val pagerInterceptionMode: Int,
     val moduleDescriptionMaxLines: Int = 4,
